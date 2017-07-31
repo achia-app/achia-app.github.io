dd455aac5dfc8a6ace6fe0a7bdbdd9f984fc79bf
Todd Lim <macone@Todd-Lim.local>
2017-08-01 01:48:53 +0700

    add webapp

---
+++ webapp/js/main.js
@@ -0,0 +1,35 @@
+var loading =
+  '<div class="windows8"><div class="wBall" id="wBall_1"><div class="wInnerBall"></div></div><div class="wBall" id="wBall_2"><div class="wInnerBall"></div></div><div class="wBall" id="wBall_3"><div class="wInnerBall"></div></div><div class="wBall" id="wBall_4"><div class="wInnerBall"></div></div><div class="wBall" id="wBall_5"><div class="wInnerBall"></div></div></div>';
+var result =
+  '<div style="width:80%"><h3 style="text-align:center">Bạn vừa gửi thành công lên server</h3><input class="form-group form-control btn btn-danger" type="submit" value="Gửi mới" id="new" /></div>';
+
+$(document).ready(function() {
+  var form = $("form");
+
+  $("form").submit(function(e) {
+    e.preventDefault();
+    var name = $("#name").val();
+    var description = $("#description").val();
+    var amount = $("#amount").val();
+
+    var formUrl =
+      "https://docs.google.com/forms/u/1/d/e/1FAIpQLScVHgg3n7o6lIsutXrkNbrYHmJ-5PwyqU6ZrKhI8uIHvEEcFA/formResponse";
+    $("main").html(loading);
+    $.post(
+      formUrl,
+      {
+        "entry.1998458345": name,
+        "entry.179782795": description,
+        "entry.1008143291": amount
+      },
+      function(data) {}
+    );
+    setTimeout(function() {
+      $("main").html(result);
+    }, 1000);
+  });
+
+  $(document).on("click", "#new", function() {
+    $("main").html(form);
+  });
+});
